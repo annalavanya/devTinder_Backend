14cceaa7349e8147ddc232c6b2b7e3ff2ac7b8a3
Anna_LavanyaT <anna.lavanya@centizen.com>
2024-10-05 15:26:54 +0530

    playing with multiple routes

--- src/app.js
+++ src/app.js
@@ -2,63 +2,36 @@ const express = require("express");
 
 const app = express();
 
-// app.use can handle all the http methods like get,post,put,patch,delete etc,..
-// app.use("/user",(req,res)=>{
-//     res.send("Succesfully done");
-// })
-
-// it handle only GET request
-app.get("/user/:userId/:name/:password",(req,res)=>{
-    console.log(req.params);
-    // console.log(req.query);
-    console.log(req.url);
-    res.send({firstName: "Anna", lastName: "Lavanya"});
-})
-
-// it handle only POST request
-app.post("/user",(req,res)=>{
-    res.send("Data Saved successfully");
-})
-
-// it handle only PUT request
-app.put("/user",(req,res)=>{
-    res.send("Successfully modified all the datas in that row");
-})
-
-// it handle only PATCH request
-app.patch("/user",(req,res)=>{
-    res.send("Successfully modified the provided datas in that row");
-})
-
-// it handle only DELETE request
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Successfully");
-})
-
-// app.use("/user",(req,res)=>{
-//     res.send("Succesfully done");
-// })
-
-// app.use("/hello/123", (req,res)=>{
-//     // console.log("req",req.headers.host);
-//     res.send("Hello 123");
-// })
-
-// app.use("/hello", (req,res)=>{
-//     // console.log("req",req.headers.host);
-//     res.send("Hello Hello");
-// })
-
-// app.use("/test", (req,res)=>{
-//     // console.log("req",req.headers.host);
-//     res.send("Test test");
-// })
-
-
-// app.use("/", (req,res)=>{   // wildcard route - /
-//     // console.log("req",req.headers.host);
-//     res.send("Welcome");
-// })
+app.use('/user', 
+    (req,res,next)=>{
+        console.log("First Response");
+        next();
+        console.log("First Response11");
+        // res.send("Response1");
+        console.log("First Response111");
+        // next();
+    },
+    (req,res,next)=>{
+        console.log("Second Response");
+        next();
+        console.log("Second Response1111");
+        // res.send("Response2");
+        // next();
+    },
+    (req,res,next) => {
+        console.log("Third Response");
+        next();
+    },
+    (req,res,next) => {
+        console.log("Fourth Response");
+        // res.send("Response4");
+        next();
+    },
+    (req,res,next) => {
+        console.log("Fifth Response");
+        // res.send("Response5");
+    }
+)
 
 app.listen(7777,()=>{
     console.log("Server is listening in port 7777");
