ecf9a018dc5505916f8a2e2a3f34669f3a00ebf7
Anna_LavanyaT <anna.lavanya@centizen.com>
2024-10-10 10:41:25 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,116 +1,30 @@
 const express = require("express");
-
+const connectDB = require('./config/database');
 const app = express();
+const User = require('./models/user');
 
-// error handling
-app.use('/',(err,req,res,next)=>{
-    if (err) {
-        res.status(500).send("Something went wrong!!!");
+app.post('/signup', async (req, res) => {
+    // creating an instance of the model
+    const user = new User({
+        firstName: "Anna",
+        lastName: "Lavanya",
+        emailId: "lavanya@gmail.com",
+        password: "lavanya@123"
+    });
+    try {
+        await user.save();
+        res.send("User Added succesfully");
+    } catch (err) {
+        res.status(400).send("Error while adding data", err.message);
     }
-})
-
-app.get('/getUserDetails', (req, res) => {
-    // try {
-        throw new Error("Error happend");
-        res.send("Hello");
-    // }
-    // catch(err) {
-    //     res.status(500).send("Something went wrong");
-    // }
 });
 
-app.use('/',(err,req,res,next)=>{
-    if (err) {
-        res.status(500).send("Something went wrong!!!");
-    }
+
+connectDB()
+.then(() => {
+    console.log("Database connection established");
+    app.listen(7777,()=>{
+        console.log("Server is listening in port 7777");
+    });
 })
-
-// middleware
-const { auth } = require('../middlewares/auth');
-const { user } = require('../middlewares/user');
-
-// app.get('/admin/getOneUser', auth, (req, res, next) => {
-//     res.send("Users send succesfully");
-// });
-
-// app.get('/admin/deleteUser', auth, (req,res,next) => {
-//     res.send("User removed successfully");
-// })
-
-// app.get('/user/getDetails', user ,(req,res,next) => {
-//     res.send("User details are fetched succesfully");
-// })
-
-// app.get('/user/login',(req,res,next)=>{
-//     res.send("Successfully logined");
-// })
-
-// route handler
-// app.use('/user', 
-//     [(req,res,next)=>{
-//         console.log("First Response");
-//         next();
-//         console.log("First Response11");
-//         // res.send("Response1");
-//         console.log("First Response111");
-//         // next();
-//     },
-//     (req,res,next)=>{
-//         console.log("Second Response");
-//         next();
-//         console.log("Second Response1111");
-//         // res.send("Response2");
-//         // next();
-//     }],
-//     [(req,res,next) => {
-//         console.log("Third Response");
-//         next();
-//     }],
-//     [(req,res,next) => {
-//         console.log("Fourth Response");
-//         // res.send("Response4");
-//         next();
-//     },
-//     (req,res,next) => {
-//         console.log("Fifth Response");
-//         // res.send("Response5");
-//         next();
-//         //  res.send("Response5");
-//     }]
-// )
-
-
-
-// app.use('/', 
-//     (req,res,next)=>{
-//         console.log("First Response");
-//         res.send("Response 1");
-//         next();
-//     }
-// );
-// app.use('/user', 
-//     (req,res,next)=>{
-//         console.log("Second 1 Response");
-//         next();
-//     },
-//     (req,res,next)=>{
-//         console.log("Second 2 Response");
-//         next();
-//     },
-//     (req,res,next)=>{
-//         console.log("Second 3 Response");
-//         // res.send("Response send");
-//         // next();
-//     }
-// );
-
-
-// error handling
-// app.get('/',(req,res)=>{
-//     throw new Error("Oops!!!");
-// })
-
-
-app.listen(7777,()=>{
-    console.log("Server is listening in port 7777");
-})
+.catch((err) => console.error("Database connection not established"));
